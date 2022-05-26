69485c4c12808bf234dc9b1216e2956bba38d764
sabbir322 <sabbirhosan370@gmail.com>
2022-05-26 13:12:34 +0600

    payment done

--- src/Pages/Home/CheckoutForm/CheckoutForm.js
+++ src/Pages/Home/CheckoutForm/CheckoutForm.js
@@ -2,29 +2,30 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import React, { useEffect, useState } from "react";
 
 const CheckoutForm = ({ orders }) => {
-  const { price } = orders;
+  const { price, customerEmail, customerName } = orders;
   const stripe = useStripe();
   const elements = useElements();
   const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [transactionId, setTransactionId] = useState("");
   const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
-    fetch('http://localhost:5000/create-payment-intent', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-        },
-        body: JSON.stringify({ price })
+    fetch("http://localhost:5000/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify({ price }),
     })
-        .then(res => res.json())
-        .then(data => {
-            if (data?.clientSecret) {
-                setClientSecret(data.clientSecret);
-            }
-        });
-
-}, [price]);
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.clientSecret) {
+          setClientSecret(data.clientSecret);
+        }
+      });
+  }, [price]);
 
   const handleSubmit = async (evetn) => {
     evetn.preventDefault();
@@ -49,6 +50,28 @@ const CheckoutForm = ({ orders }) => {
     // }
     // or
     setCardError(error?.message || "");
+
+    setSuccess("");
+    // confirm card payment
+    const { paymentIntent, error: intentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: customerName,
+            email: customerEmail,
+          },
+        },
+      });
+    if (intentError) {
+      setCardError(intentError?.message);
+      setSuccess("");
+    } else {
+      setCardError("");
+      console.log(paymentIntent);
+      setTransactionId(paymentIntent.id);
+      setSuccess("Congrats! Your Payment is completed.");
+    }
   };
   return (
     <div>
@@ -77,7 +100,15 @@ const CheckoutForm = ({ orders }) => {
           Pay
         </button>
       </form>
-      {cardError && <p className="text-red-500">{cardError}</p>}
+      {cardError && <p className="text-red-500 mt-6">{cardError}</p>}
+      {success && (
+        <div className="text-green-500 mt-6">
+          <p>{success} </p>
+          <p className="text-white">
+            Your transaction Id: <span className="text-green-500 font-bold">{transactionId}</span>{" "}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
