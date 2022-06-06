a2d89c4e6dfe6cbe1656af4c952941cb08d40261
sabbir322 <sabbirhosan370@gmail.com>
2022-06-06 15:25:10 +0600

    check new branch

--- src/Pages/Home/AboutUs/AboutUs.js
+++ src/Pages/Home/AboutUs/AboutUs.js
@@ -9,6 +9,7 @@ const AboutUs = () => {
                 <h1 className='text-center text-4xl mb-3'>About Us</h1>
                 <p>With more than 23 years in the tools business, Agile Magnetics, a Standex tools company, has set the standard for manufacturing custom-designed tools at the highest levels of quality and value. Our highly experienced team has led the industry in manufacturing quality high frequency power assemblies,  markings when required. Our highly qualified staff of engineers work together in our state-of-the-art 40,000 square foot ITAR manufacturing center in Concord, NH to ensure that every custom design project is manufactured to our customers’ exact specifications and meet virtually any specification. To ensure we deliver only the highest quality products to our customers, every product is computer tested prior to delivery.</p>
                 <button className='btn  rounded-3xl px-6 btn-sm mt-4 text-white'>See More</button>
+                <h1>change text in my new repository</h1>
             </div>
             <div className='lg:w-1/2 mt-4 lg:mt-0 pl-5'>
                 <img className='rounded' src={aboutUsImg} alt="" />
